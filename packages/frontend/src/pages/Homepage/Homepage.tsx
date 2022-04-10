ba4b93c6d454648d50c2f3d7ceebb1917953e896
Joshua <joshuanazareth97@gmail.com>
2022-04-10 09:39:48 +0530

    fix hompage scaffold

--- packages/frontend/src/pages/Homepage/Homepage.tsx
+++ packages/frontend/src/pages/Homepage/Homepage.tsx
@@ -23,58 +23,9 @@ const Homepage = (props: unknown) => {
 						marginBottom: '1rem',
 					}}
 				>
-					LiquiNFT
+					CFY
 				</Typography>
-				<Typography
-					variant="h5"
-					sx={{
-						marginBottom: '0.5rem',
-					}}
-				>
-					A Zilliqa based NFT fractionalization platform.
-				</Typography>
-				<Typography
-					fontSize="1rem"
-					color="darkGrey"
-					sx={{
-						marginBottom: '2rem',
-					}}
-				>
-					LiquiNFT contracts allow users to lock up their NFT in the LiquiNFT contract, which then creates a new
-					fungible token representing fractional ownership of the NFT. <br />
-					The requirement for redemption is simply that the user's balance of the fractional shares is exactly equal to
-					the total supply of the fractional shares, at the time of redemption.{' '}
-				</Typography>
-				<Box
-					sx={{
-						display: 'grid',
-						gridTemplateColumns: 'repeat(2, 1fr)',
-						gap: 3,
-					}}
-				>
-					<Button
-						// onClick={async () => await zilPay.wallet.connect()}
-						// href=""
-						variant="contained"
-						color="primary"
-					>
-						Get Started
-					</Button>
-					<Button href="https://github.com/FoundationCryptoLabs/LiquiNFT" variant="outlined" color="primary">
-						Read the whitepaper
-					</Button>
-				</Box>
 			</Box>
-			<Box
-				flexBasis="50%"
-				display="flex"
-				justifyContent="center"
-				sx={{
-					'& img': {
-						width: '80%',
-					},
-				}}
-			></Box>
 		</Box>
 	);
 };
